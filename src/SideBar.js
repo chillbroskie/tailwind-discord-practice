1d154a67d1c21d1d6492a8eb7db0c98d429ff9b2
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2022-01-08 21:53:14 -0500

    fixed

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from "react";
 import { BsFillLightningFill, BsPlus } from "react-icons/bs";
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { FaFire, FaPoo } from "react-icons/fa";
 
 const SideBar = () => {
   return (
-    <div className="Fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
+    <div className="top-0 left-0 flex flex-col w-16 h-screen m-0 shadow-lg Fixed bg-primary text-secondary">
       <SideBarIcon icon={<FaFire size="28" />} />
       <SideBarIcon icon={<BsPlus size="28" />} />
       <SideBarIcon icon={<FaPoo size="28" />} />
       <SideBarIcon icon={<BsFillLightningFill size="28" />} />
     </div>
-  )
+  );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip  ' }) => (
-  <div className="sidebar-icon group"> 
+const SideBarIcon = ({ icon, text = "tooltip  " }) => (
+  <div className="sidebar-icon group">
     {icon}
 
-    <span className="sidebar-tooltip group-hover:scale-100">
-      {text}
-    </span>
-
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
   </div>
 );
 
-export default SideBar
+export default SideBar;
